1a9d63b45b909926e1e7103135868f31342ad3b1
n0thing2c <trannguyenduythinhhvn@gmail.com>
2026-06-19 19:47:17 +0700

    feat: create frame of alert event

---
+++ backend/internal/processing/producer/redpandaAlertProducer.go
@@ -0,0 +1,18 @@
+package producer
+
+import (
+	"context"
+
+	sharedDomain "github.com/n0thing2c/Soigineer/internal/shared/domain"
+)
+
+type RedpandaAlertProducer struct {
+}
+
+func NewAlertProducer() *RedpandaAlertProducer {
+	return &RedpandaAlertProducer{}
+}
+
+func(p *RedpandaAlertProducer) ProduceAlert(ctx context.Context, log sharedDomain.AlertEvent) error {
+	return nil
+}
---
+++ backend/internal/shared/domain/alertEvent.go
@@ -0,0 +1,11 @@
+package domain
+
+type AlertEvent struct {
+
+}
+
+func ToAlertEvent(event RawLogEvent) AlertEvent {
+	return AlertEvent{
+		
+	}
+}
