6bdd878107848b9ce4a19c93c2d9d88022527f44
n0thing2c <trannguyenduythinhhvn@gmail.com>
2026-06-23 22:39:30 +0700

    format file to gofmt

--- backend/internal/shared/domain/alertEvent.go
+++ backend/internal/shared/domain/alertEvent.go
@@ -3,11 +3,11 @@ package domain
 import "time"
 
 type AlertEvent struct {
-	EventID         string `json:"eventId"`
-	ApplicationName string `json:"applicationName"`
-	Level           string `json:"level"`
-	Message         string `json:"message"`
-	Fingerprint     string `json:"fingerprint"`
-	TraceID         string `json:"traceId"`
+	EventID         string    `json:"eventId"`
+	ApplicationName string    `json:"applicationName"`
+	Level           string    `json:"level"`
+	Message         string    `json:"message"`
+	Fingerprint     string    `json:"fingerprint"`
+	TraceID         string    `json:"traceId"`
 	Timestamp       time.Time `json:"timestamp"`
 }
